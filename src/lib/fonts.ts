dad6eec69246902f972dae70363fd8a97e8e9261
Gokalp Caycı <gklpcayci@gmail.com>
2023-07-10 15:02:07 +0300

    initial commit

---
+++ src/lib/fonts.ts
@@ -0,0 +1,20 @@
+import localFont from "next/font/local";
+import {
+  JetBrains_Mono as FontMono,
+  Inter as FontSans,
+} from "next/font/google";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+
+export const fontHeading = localFont({
+  src: "../assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+});
